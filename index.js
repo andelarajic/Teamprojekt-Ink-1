ebfdf1bc86aa470987b298ec9861292ea0e22fd6
andjelarajic <andjelarajic1@gmail.com>
2020-11-24 15:27:52 +0100

    guill editor

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+
+var quill = new Quill('#editor-container', {
+    modules: {
+      toolbar: [
+        [{ header: [1, 2, false] }],
+        ['bold', 'italic', 'underline'],
+        ['image', 'code-block']
+      ]
+    },
+    placeholder: 'Compose an epic...',
+    theme: 'snow'  // or 'bubble'
+  });
