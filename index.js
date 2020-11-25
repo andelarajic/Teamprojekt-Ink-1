ee994c96e5fba43cc88f62287d5169be74c23800
kayzersozee <63726574+kayzersozee@users.noreply.github.com>
2020-11-25 10:56:11 +0100

    editor added

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
+    var x = document.getElementById("myTopnav1");
+    if (x.className === "topnav1") {
         x.className += " responsive";
     } else {
-        x.className = "topnav";
+        x.className = "topnav1";
     }
 }
+/* var options = {
+    //debug: 'info',
+    modules: {
+        toolbar: '#toolbar'
+    },
+    placeholder: 'Compose an epic...',
+    readOnly: false,
+    theme: 'snow'
+};
+var editor = new Quill('#editor', options); */
+
+var quill = new Quill('#editor', {
+    theme: 'snow'
+});
