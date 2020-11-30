9b49e7c3dda4844c19b5aac64296f37c548a4517
Apotheosiz <sascha.kasapovski@student.kyh.se>
2020-11-30 15:53:49 +0100

    code update by Big K

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
+// Get DOM Elements
+const modal = document.querySelector('#my-modal');
+const modalBtn = document.querySelector('#modal-btn');
+const closeBtn = document.querySelector('.close');
+
+// Events
+modalBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+window.addEventListener('click', outsideClick);
+
+// Open
+function openModal() {
+    modal.style.display = 'block';
+}
+
+// Close
+function closeModal() {
+    modal.style.display = 'none';
+}
+
+// Close If Outside Click
+function outsideClick(e) {
+    if (e.target == modal) {
+        modal.style.display = 'none';
+    }
+}
+function myFunction() {
+    let x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
 
  /*var options = {
     //debug: 'info',
@@ -32,6 +66,8 @@ let quill = new Quill('#editor', {
 });
 
 function printDiv() {
+    window.print()
+    return;
     var divContents = document.getElementById("editor").innerHTML;
     var a = window.open('', '', 'height=500, width=500');
     a.document.write('<html>');
