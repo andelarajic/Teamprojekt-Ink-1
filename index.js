b3dcae14101eabc24574ad8f34b260b636c9aad9
andjelarajic <andjelarajic1@gmail.com>
2020-12-11 14:05:13 +0100

    theme button to navbar

--- index.js
+++ index.js
@@ -159,14 +159,14 @@ let toolbarOptions = [
     ["blockquote", "code-block"],
     [{ "header": [1, 2, 3, 4, 5, 6, false] }],
     [{ "list": "ordered" }, { "list": "bullet" }],
-    [{ "script": "sub" }, { "script": "super" }],
+    /*[{ "script": "sub" }, { "script": "super" }],
     [{ "indent": "-1" }, { "indent": "+1" }],
     [{ "direction": "rtl" }],
     [{ "size": ["small", false, "large", "huge"] }],
     ["link", "image", "video", "formula"],
     [{ "color": [] }, { "background": [] }],
     [{ "font": [] }],
-    [{ "align": [] }],
+    [{ "align": [] }],*/
 ];
 let quill = new Quill('#editor', {
     modules: {
