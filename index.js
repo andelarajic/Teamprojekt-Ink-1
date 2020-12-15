f768208e1aec15f3c3be2c99a450e7cf05009335
kayzersozee <63726574+kayzersozee@users.noreply.github.com>
2020-12-15 23:09:06 +0100

    darkmode, favourite function, responsive design fixed

--- index.js
+++ index.js
@@ -9,9 +9,23 @@ const navUL = document.querySelector('.navbar-nav');
 const mainDIV = document.querySelector('#main');
 const favBtn = document.querySelector('.favSecBtn');
 const noteListElement = document.querySelector(".note-list");
+let showingFavs = false;
+
+favBtn.addEventListener('click', (evt) => {
+    showingFavs = !showingFavs;
+    noteListElement.style.display = "block";
+    if (showingFavs) {
+        renderNotes(notes.filter(note => note.favourite))
+    } else {
+        renderNotes(notes);
+    }
+})
 
 //Hämta alla notes som finns sparade i local-storage
 let notes = JSON.parse(localStorage.getItem("notes"))
+/* if (notes) {
+    popup.style.display = 'none';
+} */
 //ID på den note som för tillfället används
 let currentNoteId = generate_UUID()
 
@@ -26,21 +40,23 @@ console.log("currentNoteId: " + currentNoteId) //TODO ta bort
 
 function createNote() {
     console.log("title: " + inpTitle.innerText.length + ", text: " + inpTitle.innerText.length + ", number of notes: " + notes.length) //TODO ta bort
-   /* var x = inpTitle.innerText;
-    if (x == "" || x.length <= 15) {
+    var x = inpTitle.innerText;
+    let y = inpEditor.innerText; // vrf funkar den ej
+    if (y = '' || x.length >= 15) { // Fråga kristian om = och inte ==
         alert("You need to fill out the name field properly");
-        return false;*/
+        return false;// För att ha en titel med max 15 bokstäver 
+    }
     //Skapa upp en Note med hjälp av fält från HTML
-    if(inpTitle.innerText.length < 1 && quill.getText.length < 1) return //Detta för att inte kunna skapa en note utan titel eller text
+    if (inpTitle.innerText.length < 1 && quill.getText.length < 1) return //Detta för att inte kunna skapa en note utan titel eller text
 
     const noteExists = notes.length > 0 ? isNoteIdInList(notes, currentNoteId) : false;
 
     console.log("Does note exist: " + noteExists)
-    
-    if(noteExists){
+
+    if (noteExists) {
         notes.forEach(note => {
             console.log("searching for note with id: " + currentNoteId + ", currently at note: " + note.id)
-            if(note.id == currentNoteId) {
+            if (note.id == currentNoteId) {
                 console.log("Note with searched id found: " + currentNoteId)
                 note.date = new Date().toUTCString()
                 note.title = inpTitle.innerText
@@ -48,7 +64,7 @@ function createNote() {
             }
         })
     } else {
-        console.log("No Note with searched id: "+ currentNoteId + " found, creating a new note")
+        console.log("No Note with searched id: " + currentNoteId + " found, creating a new note")
         currentNoteId = generate_UUID()
         var note = {
             id: currentNoteId,
@@ -68,7 +84,7 @@ function createNote() {
 }
 
 //Lägg till/Skriv över notes
-function saveNotesToLocalStorage(notes) {    
+function saveNotesToLocalStorage(notes) {
     //Omvandlar notes till JSON-format och sparar sedan ner innehållet till local-storage med nyckeln notes
     localStorage.setItem('notes', JSON.stringify(notes))
 }
@@ -81,21 +97,28 @@ function renderNotes(notes) {
     notes.forEach(note => {
         //Gör om javascripts note till HTML note, lägg sedan till det som ett child på note-list elemetet i HTML
         noteListElement.appendChild(noteToHTML(note));
-    })
+    });
 }
 
 //Gör om note i javascript till HTML format
 function noteToHTML(note) {
     //Skapa ett list-item
-    let container = document.createElement('div');
+    let container = document.createElement('li');
     //Lägg till id från note som data-id attribut
     container.setAttribute('data-id', note.id);
     //Lägg till den skapade html note strukturen som list-items innre presentation
     container.innerHTML = noteHtmlStructure(note)
-    container.onclick = function () {
-        currentNoteId = note.id
-        inpTitle.innerText = note.title
-        quill.setText(note.text)
+    container.onclick = function (evt) {
+        //let clickedLI = evt.target.closest('li');
+        //let clickedID = clickedLI.getAttribute('data-id');
+        if (evt.target.classList.contains('fa-star') || evt.target.classList.contains('fa-star-o')) {
+            //console.log(note.id)
+            toggleFav(note.id)
+        } else {
+            currentNoteId = note.id
+            inpTitle.innerText = note.title
+            quill.setText(note.text)
+        }
     }
     return container
 }
@@ -103,34 +126,34 @@ function noteToHTML(note) {
 //Med hjälp av javascript note, returnera strukturerat html för en note
 function noteHtmlStructure(note) {
     //Bygger upp en note som ska presenteras för användaren i HTML format
-    return `<div class="card"><div class="remove"></div><p class="left"><b>${shortenText(note.title, 15)}</b><span class="right">${note.date}</span></p><p>${shortenText(note.text, 200)}</p></div>`
+    return `<div class="card"><div class="remove"></div><p class="left"><b>${shortenText(note.title, 15)}</b><span class="right">${note.date}</span></p><i class="fa fa-star${note.favourite ? '' : '-o'}" aria-hidden="true"></i><p>${shortenText(note.text, 200)}</p></div>`
 }
 
 //Om text behöver kortas ner så gör det och lägg till "..." på slutet för att indikera att det finns mer text 
-function shortenText(text, characters) {
+function shortenText(text, characters) {
     if (text.length > characters) {
-        text = text.substring(0, characters)+'...';
+        text = text.substring(0, characters) + '...';
     }
     return text
 }
 
 //Funktion för att generera ett unikt id som används vi skapande av note.
-function generate_UUID(){
+function generate_UUID() {
     var dt = new Date().getTime();
-    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = (dt + Math.random()*16)%16 | 0;
-        dt = Math.floor(dt/16);
-        return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = (dt + Math.random() * 16) % 16 | 0;
+        dt = Math.floor(dt / 16);
+        return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
     });
     return uuid;
 }
 
 //Kolla om currentNoteId finns i listan av notes
-function isNoteIdInList(notes, id){
-    var exists = false
+function isNoteIdInList(notes, id) {
+    let exists = false
     notes.forEach(note => {
-        console.log("noteId: "+ note.id + " - currentNodeId: " + id)
-        if(note.id == id) {
+        console.log("noteId: " + note.id + " - currentNodeId: " + id)
+        if (note.id == id) {
             exists = true
         }
     })
@@ -138,13 +161,13 @@ function isNoteIdInList(notes, id){
 }
 
 //Rensa title, text och generera ett nytt currentNoteId
-function clearEditor(){
+function clearEditor() {
     currentNoteId = generate_UUID()
     inpTitle.innerText = ""
     quill.setText("")
 }
 
-function removeNote(id){
+function removeNote(id) {
     console.log(id)
 }
 
@@ -155,10 +178,10 @@ function saveNote() {
 }
 
 function removeNote() {
-    var noteExists = isNoteIdInList(notes, currentNoteId);
-    if(noteExists){
-        for(var i = 0; i < notes.length; i++){
-            if(notes[i].id == currentNoteId){
+    let noteExists = isNoteIdInList(notes, currentNoteId);
+    if (noteExists) {
+        for (let i = 0; i < notes.length; i++) {
+            if (notes[i].id == currentNoteId) {
                 notes.splice(i, 1);
             }
         }
@@ -177,12 +200,27 @@ noteListElement.addEventListener('click', function (sideBar) {
         noteObj.favourite = !noteObj.favourite;
     }
 });
+// collapsible
+let savedNotes = document.getElementsByClassName("savedNotes");
+let i;
 
+for (i = 0; i < savedNotes.length; i++) {
+    savedNotes[i].addEventListener("click", function () {
+        let content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+
+}
+// collapsible slut
 function toggleFav(id) {
-    inpTitle.innerText = notesArr.find(note => note.id == id).title
-    let noteObj = inpTitle.innerText;
+    let noteObj = notes.find(note => note.id == id)
     noteObj.favourite = !noteObj.favourite;
-    /* saveNotes(); */
+    saveNotesToLocalStorage(notes);
+    renderNotes(notes)
 }
 
 //POPUP Funktioner 
@@ -218,10 +256,22 @@ function myFunction() {
         x.className = "topnav";
     }
 }
-
+/* 
 function openNav() {
+    if (window.innerWidth < 600) {
+        return false;
+    }
+    // //Varför fungerar ej den här? fråga kristian
     document.getElementById("sideNav").style.display = "block";
-}
+
+} */
+//document.addEventListener('animationEnd', dispatchEvent, false);// funkar ej fråga kristian
+
+// check the animation name and operate accordingly
+//function dispatchEvent(event) {
+//    if (event.animationName === 'max-width-600px') {
+//       document.body.innerHTML = 'max width is 600px';
+//    }
 
 /* Close/hide the sidenav */
 function closeNav() {
@@ -230,12 +280,47 @@ function closeNav() {
 
 navUL.addEventListener('mouseenter', function (evt) {
     //console.log("MOUSE is on nav!")
-    mainDIV.style = "margin-left: 200px;"
+    if (window.innerWidth >= 900) {
+        mainDIV.style = "margin-left: 200px;"
+    }
+    //console.log(window.innerWidth);
 })
 navUL.addEventListener('mouseleave', function (evt) {
     //console.log("MOUSE is off nav!")
     mainDIV.style = "";
 })
+
+/* let infoBtn = document.getElementByClassName('link-text');
+ 
+infoBtn.addEventListener('click', function () {
+    openPopup()
+}); */
+
+let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+// add class 'fav' to each favorite
+favorites.forEach(function (favorite) {
+    document.getElementById(favorite).className = 'fav';
+});
+// register click event listener
+document.querySelector('.note-list').addEventListener('click', function (e) {
+    var id = e.target.id,
+        item = e.target,
+        index = favorites.indexOf(id);
+    // return if target doesn't have an id (shouldn't happen)
+    if (!id) return;
+    // item is not favorite
+    if (index == -1) {
+        favorites.push(id);
+        item.className = 'fav';
+        // item is already favorite
+    } else {
+        favorites.splice(index, 1);
+        item.className = '';
+    }
+    // store array in local storage
+    localStorage.setItem('favorites', JSON.stringify(favorites));
+});
+
 // NAV FUNKTIONER SLUT
 
 // QUILL FUNKTIONER 
@@ -272,7 +357,7 @@ function printDiv() {
 // PRINT FUNKTIONER SLUT
 
 /* function fromStorage() {
-
+ 
     let notesArrStr = localStorage.getItem(activeNoteID, JSON.stringify({ title: savedTitle, content: savedEditor }));
     if (!notesArrStr) {
         return;
@@ -295,4 +380,17 @@ const chk = document.getElementById('chk');
 
 chk.addEventListener('change', () => {
     document.body.classList.toggle('dark');
+    document.btn.style.color = 'white';
 });
+
+var checkbox = document.querySelector('input[name=chk]');
+
+checkbox.addEventListener('change', function () {
+    if (this.checked) {
+
+        document.documentElement.setAttribute('data-theme', 'dark')
+    } else {
+
+        document.documentElement.setAttribute('data-theme', 'light')
+    }
+})
